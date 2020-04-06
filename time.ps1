aed668b8f12258948cf5a7c775e025f42dd3746e
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2020-04-06 13:41:14 +0800

    export to csv

--- time.ps1
+++ time.ps1
@@ -1,4 +1,4 @@
-$time = Get-CWMTimeEntry -Condition 'member/identifier="gavin" and dateEntered > [2020-2-27T05:53:27Z]' -all
+$time = Get-CWMTimeEntry -Condition 'dateEntered > [2020-4-1T01:00:00Z]' -all
 $hours = 0
 $companyHours = @()
 
@@ -6,21 +6,48 @@ foreach($entry in $time){
 	$hours = $hours + $entry.actualhours
 	
 	$companyname = $entry.company.name
+	$ticketID = $entry.chargetoid
 	$hour = $entry.actualhours
 	$name = $entry.member.identifier
+	$chargetotype = $entry.chargetotype
+	$worktype = $entry.worktype.name
+	$workrole = $entry.workrole.name
+	$agreement = $entry.agreement.name
+	$billableoption = $entry.billableoption
+	$notes = $entry.notes
+	$hoursbilled = $entry.hoursbilled
+	$hourlyrate = $entry.hourlyrate
 	
-	$item = "" | Select name,company,hours
-	$item.name = $name + "|"
-	$item.company = $companyname + "|"
-	$item.hours = $hour
+	if($notes)
+	{
+		$notes = $notes.replace("`n",", ").replace("`r",", ")
+	}
+	
+	$item = "" | Select ticketID,chargetotype,worktype,workrole,agreement,billableoption,name,company,hours,hoursbilled,hourlyrate,notes
+	$item.ticketID = $ticketID.ToString() 
+	$item.chargetotype = $chargetotype 
+	$item.worktype = $worktype 
+	$item.workrole = $workrole 
+	$item.agreement = $agreement 
+	$item.billableoption = $billableoption 
+	$item.name = $name 
+	$item.company = $companyname 
+	$item.hours = $hour.ToString() 
+	$item.hoursbilled = $hoursbilled.ToString() 
+	$item.hourlyrate = $hourlyrate.ToString() 
+	$item.notes = $notes
+	
 	
 	$companyHours += $item
 	
 }
 
-$out = $companyHours.GetEnumerator()|sort -property value -Descending
+$companyhours | Export-Csv -Path .\hours.csv
 
-$out = $out|Format-Table -Autosize -HideTableHeaders| out-string
-Add-Content -Path .\hours.txt -Value $out
+#$out = "ticketID|chargetotype|worktype|workrole|agreement|billableoption|name|company|hours|hoursbilled|hourlyrate|notes"
+#Add-Content -Path .\hours.txt -Value $out
 
-(gc .\hours.txt) | ? {$_.trim() -ne "" } | set-content .\hours.txt
+#$out = $companyhours|Format-Table -Property * -autosize |out-string -width 10000
+#Add-Content -Path .\hours.txt -Value $out
+
+#(gc .\hours.txt) | ? {$_.trim() -ne "" } | set-content .\hours.txt
